9d38282d43a2d815f9f490c27754edff741ed27f
Pedro Teles <pedroateles@protonmail.com>
2020-08-30 14:50:19 -0300

    added picasso dependency

--- app/src/main/java/dev/pedroteles/githubapi/app/feature/userinfo/view/UserInfoActivity.kt
+++ app/src/main/java/dev/pedroteles/githubapi/app/feature/userinfo/view/UserInfoActivity.kt
@@ -2,9 +2,13 @@ package dev.pedroteles.githubapi.app.feature.userinfo.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
 import dev.pedroteles.githubapi.R
 import dev.pedroteles.githubapi.app.feature.userinfo.viewmodel.UserInfoViewModel
 import kotlinx.android.synthetic.main.activity_user_info.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 
 class UserInfoActivity : AppCompatActivity() {
@@ -25,5 +29,9 @@ class UserInfoActivity : AppCompatActivity() {
         txtUrl.text = dto.url
         txtLocation.text = dto.location
         txtId.text = dto.id.toString()
+
+        CoroutineScope(Dispatchers.Main).launch {
+            Picasso.get().load(dto.avatarUrl).into(imgUser)
+        }
     }
 }
